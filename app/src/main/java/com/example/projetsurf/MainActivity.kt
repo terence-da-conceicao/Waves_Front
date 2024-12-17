0b95a6da94d27289a62791f40f3e43eb6c2635d1
Paul Vaucher <araghast@gmail.com>
2024-12-17 16:43:36 +0100

    ajout texte et placeholder image

--- app/src/main/java/com/example/projetsurf/MainActivity.kt
+++ app/src/main/java/com/example/projetsurf/MainActivity.kt
@@ -5,15 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.projetsurf.ui.theme.ProjetSurfTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,16 +43,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen () {
-    Box(modifier = Modifier.fillMaxSize()) {
-    Image (
-        painter = painterResource(id = R.drawable.sand_back),
-        contentDescription  = "Demo background",
-        contentScale = ContentScale.FillBounds,
-        modifier = Modifier.matchParentSize()
-    )
-    }
+        Box(modifier = Modifier.fillMaxSize()) {
+        Image (
+            painter = painterResource(id = R.drawable.sand_back),
+            contentDescription  = "Demo background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+        Column  (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = "Spot", fontSize = 30.sp, fontWeight = FontWeight.Medium, modifier = Modifier.padding(all = 20.dp))
+            Image(painter = painterResource(id = R.drawable.test_default),
+                contentDescription  = "Image placeholder",)
+            Text(text = "Lieu", fontSize = 30.sp, fontWeight = FontWeight.Medium, modifier = Modifier.padding(all = 20.dp))
+        }
+        }
 
-}
+    }
 
 @Preview(showBackground = true)
 @Composable
