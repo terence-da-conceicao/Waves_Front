86619afa89bd129fe9ab3c9fe120b4a555d78983
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2024-12-18 15:25:48 +0100

    première version de l'encard spot

--- app/src/main/java/com/example/projetsurf/MainActivity.kt
+++ app/src/main/java/com/example/projetsurf/MainActivity.kt
@@ -5,60 +5,109 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.projetsurf.ui.theme.ProjetSurfTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-           ProjetSurfTheme {
-                    Surface (modifier = Modifier.fillMaxSize, painter = painterResource(id = R.drawable.sand_back)(),
-                        //color = MaterialTheme.colorScheme.background
+            //base visuelle de l'app (activité) : ProjetSurfTheme
+            ProjetSurfTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    //image de fond
+                    Image(
+                        modifier = Modifier.matchParentSize(),
+                        painter = painterResource(id = R.drawable.sand_back),
+                        contentScale = ContentScale.Crop,
+                        contentDescription = "Image de fond"
                     )
-                    {
-                        DemoScreen (theSpot)
-                    }
-                }
-            }
-        }
-    }
+                    //CardSpot(theSpot)
+                }//box
+            }//projetsurftheme
+        }//setContent
+    }//override onCreate
+}//mainactivity
+
+
+
 
 data class InfosSpot(val nom: String, val lieu: String)
 
-val theSpot = InfosSpot("Le lac des oiseaux", "Inde")
+val theSpot = InfosSpot("Bells Beach", "Australie")
 
+
+// fragment : le spot
 @Composable
-fun DemoScreen (spot:InfosSpot) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Image (
-            ,
-            contentDescription  = "Demo background",
-            contentScale = ContentScale.FillBounds,
-            modifier = Modifier.matchParentSize()
-        )
-        Column {
-            Text(text = spot.nom)
-            Text(text = spot.lieu)
+fun CardSpot(spot: InfosSpot) {
+    Surface(
+        modifier = Modifier
+            .height(300.dp)
+            .width(300.dp),
+           // .padding(20.dp),
+        color = Color(0xFFA1E2EB),
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(all = 10.dp),
+                text = spot.nom,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Medium,
+                //verticalArrangement = Arrangement.Center,
+               // horizontalAlignment = Alignment.CenterHorizontally
+            )
+            Image(
+                painter = painterResource(id = R.drawable.bellsbeach),
+                contentDescription = "photo du lieu"
+            )
+            Text(
+                modifier = Modifier
+                    .padding(all = 10.dp),
+                text = spot.lieu,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Medium
+            )
         }
     }
 }
 
+
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ProjetSurfTheme {
-        DemoScreen(theSpot)
-    }
-}
+    fun ProjetSurfTheme() {
+        CardSpot(theSpot)
+   }
+
