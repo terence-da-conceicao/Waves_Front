0ce87091bed01e6c764901056472a78fcef2728c
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2025-01-06 10:48:18 +0100

    CardSpot est bien aligné sur le background de MainActivity

--- app/src/main/java/com/example/projetsurf/MainActivity.kt
+++ app/src/main/java/com/example/projetsurf/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -55,7 +56,18 @@ fun ProjetSurfTheme () {
             modifier = Modifier
                 .matchParentSize()
         )
-        CardSpot(theSpot)
+//        Row(verticalAlignment = Alignment.CenterVertically) {
+//            Column(
+//                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            )
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+                CardSpot(theSpot)
+        }
     }
 }
 
